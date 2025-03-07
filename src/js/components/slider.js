39406926746126d0218946a2c6ade49994c2092f
Margarita <margarita0work@gmail.com>
2025-03-07 14:28:25 +0200

    Created advantage section

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -5,19 +5,20 @@ import 'swiper/scss/autoplay';
 import { Autoplay, FreeMode } from 'swiper/modules';
 
 export const useHeroSlider = () => {
-  // Инициализация слайдера только после того, как DOM загружен
   document.addEventListener('DOMContentLoaded', function () {
     new Swiper('.hero__slider', {
       modules: [FreeMode, Autoplay],
       spaceBetween: 20,
       slidesPerView: 'auto',
       loop: true,
-      speed: 2000,
+      speed: 5000,
       freeMode: true,
       autoplay: {
         delay: 0,
         disableOnInteraction: false,
       },
+      simulateTouch: false,
+      touchStartPreventDefault: true,
     });
   });
 };
